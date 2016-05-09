671d7ce7b2ffac46aeefb3c612cc21f3ec11ce3c
xiaoxuTW <xiaoxu@thoughtworks.com>
2016-05-09 16:59:32 +0800

    clean up

--- Workshop-MAY-2016/brand-server/src/main/java/com/example/model/SourceEnum.java
+++
@@ -1,8 +0,0 @@
-package com.example.model;
-
-/**
- * Created by xiaoxu on 4/28/16.
- */
-public enum SourceEnum {
-    JD, TB
-}
--- Workshop-MAY-2016/webapp/src/main/java/com/server/model/SourceEnum.java
+++
@@ -1,9 +0,0 @@
-package com.server.model;
-
-/**
- * Created by xiaoxu on 4/28/16.
- */
-public enum  SourceEnum {
-    BRAND, MOD
-}
-
